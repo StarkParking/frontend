ff722321d35537e05a9a938f83ee211489dcc982
Anh Hoang Nguyen <hoangkianh31592@gmail.com>
2024-11-07 17:05:17 +0700

    add strk address

--- src/pages/Home/Balance.tsx
+++ src/pages/Home/Balance.tsx
@@ -6,6 +6,7 @@ function Balance() {
   const { address } = useAccount()
   const { data, isFetching, isSuccess, error, status } = useBalance({
     address, //: addZeroAfter0x(address || ''),
+    token: '0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d',
     watch: false
   })
 
