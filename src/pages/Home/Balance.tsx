fb558273ac9d50adbfb355939d1f7c9669fdc2d4
Anh Hoang Nguyen <hoangkianh31592@gmail.com>
2024-11-08 09:19:54 +0700

    fix: loading balance

--- src/pages/Home/Balance.tsx
+++ src/pages/Home/Balance.tsx
@@ -5,7 +5,7 @@ import { useBalance } from '@starknet-react/core'
 
 function Balance() {
   const { address } = useAccount()
-  const { data, isFetching, isSuccess, status } = useBalance({
+  const { data, isLoading, isSuccess } = useBalance({
     address: addZeroAfter0x(address || ''),
     token: '0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d',
     watch: false
@@ -16,8 +16,7 @@ function Balance() {
       <div>
         <div className="text-sm opacity-80 mb-2">Your Balance</div>
         <div className="text-3xl font-bold">
-          {isFetching && <Dots />}
-          {isSuccess && <span>{data?.formatted} STRK</span>}
+          {isLoading ? <Dots /> : <span>{data?.formatted} STRK</span>}
         </div>
       </div>
     </div>
